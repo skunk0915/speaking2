3ff3506edd2435ad6ede442476b422575b9b093d
mizy <skunk0915@gmail.com>
2026-05-18 07:00:01 +0900

    保存アイコンをブックマークアイコンに

--- index.php
+++ index.php
@@ -291,9 +291,7 @@
                     </button>
                     <button class="btn-save" title="復習に保存">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v13a2 2 0 0 1-2 2z"></path>
-                            <polyline points="17 21 17 13 7 13 7 21"></polyline>
-                            <polyline points="7 3 7 8 15 8"></polyline>
+                            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                         </svg>
                     </button>
                     <button class="btn-history hidden" title="過去の添削を閲覧">
